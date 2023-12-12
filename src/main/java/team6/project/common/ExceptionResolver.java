5792bc0de2a7cf87f27d4d665671fdfce39e35a5
devYule <privateDevYule@gmail.com>
2023-12-13 07:08:40 +0900

    2023-12-12 by Hyunmin TodoService 소량 수정

--- src/main/java/team6/project/common/ExceptionResolver.java
+++ src/main/java/team6/project/common/ExceptionResolver.java
@@ -1,5 +1,6 @@
 package team6.project.common;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import team6.project.common.exception.BadDateInformationException;
@@ -10,27 +11,32 @@ import team6.project.common.model.ExceptionResultVo;
 
 import static team6.project.common.Const.*;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionResolver {
 
 
     @ExceptionHandler
     public ExceptionResultVo noSuchDataException(NoSuchDataException e) {
+        log.info("message = {}", e.getMessage(), e);
         return new ExceptionResultVo(NO_SUCH_DATA_EX_MESSAGE);
     }
 
     @ExceptionHandler
     public ExceptionResultVo badDateInformationException(BadDateInformationException e) {
+        log.info("message = {}", e.getMessage(), e);
         return new ExceptionResultVo(BAD_DATE_INFO);
     }
 
     @ExceptionHandler
     public ExceptionResultVo badInformationException(BadInformationException e) {
+        log.info("message = {}", e.getMessage(), e);
         return new ExceptionResultVo(BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ExceptionResultVo todoIsFullException(TodoIsFullException e) {
+        log.info("message = {}", e.getMessage(), e);
         return new ExceptionResultVo(TODO_IS_FULL_EX_MESSAGE);
     }
 
