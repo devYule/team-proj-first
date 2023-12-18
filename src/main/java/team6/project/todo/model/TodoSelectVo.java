6b648d49c4e1786cd48498d5f3b20b683f1f229a
jo <yuyule20@naver.com>
2023-12-18 17:50:02 +0900

    2023-12-18

--- src/main/java/team6/project/todo/model/TodoSelectVo.java
+++ src/main/java/team6/project/todo/model/TodoSelectVo.java
@@ -11,11 +11,11 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TodoSelectVo {
-    @Schema(title = "감정",  description = "선택된 날짜의 감정")
+    @Schema(title = "감정",  description = "선택된 날짜의 감정", defaultValue = "1")
     private Integer emotion;
-    @Schema(title = "이모션 태그",  description = "선택된 날짜의 이모션 태그")
+    @Schema(title = "이모션 태그",  description = "선택된 날짜의 이모션 태그", defaultValue = "기쁜")
     private String emotionTag;
-    @Schema(title = "조회된 투두의 PK & 내용",  description = "조회된 투두의 PK 와 투두의 내용")
+    @Schema(title = "조회된 투두의 PK & 내용",  description = "조회된 투두의 PK 와 투두의 내용 (itodo: 투두의 PK, todoContent: 투두 내용)")
     private List<TodoInfo> todos;
 
 }
