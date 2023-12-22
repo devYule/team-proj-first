22ab54c4c4875f90f1f79a02edcdf3473e536a7c
jo <yuyule20@naver.com>
2023-12-22 14:57:38 +0900

    2023-12-22

--- src/main/java/team6/project/emotion/EmotionService.java
+++ src/main/java/team6/project/emotion/EmotionService.java
@@ -1,4 +1,5 @@
 package team6.project.emotion;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,12 +9,14 @@ import team6.project.common.exception.BadInformationException;
 import team6.project.common.exception.NoSuchDataException;
 import team6.project.common.utils.CommonUtils;
 import team6.project.emotion.model.*;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import static java.time.temporal.ChronoField.DAY_OF_WEEK;
 
 
@@ -27,28 +30,28 @@ public class EmotionService {
 
     //(일 별)이모션 단계,이모션태그 insert//
     public ResVo postEmo(EmotionInsDto dto) {
-        Integer checkIuser=emotionRepository.checkIuser(dto.getIuser());
-        if(checkIuser==null){
+        Integer checkIuser = emotionRepository.checkIuser(dto.getIuser());
+        if (checkIuser == null) {
             throw new NoSuchDataException("올바른 iuser 값을 보내주세요");
         }
         //일단위 중복 체크.
-        LocalDate datetoday=LocalDate.now();
-        EmotionDuplicationDto duplication=new EmotionDuplicationDto();
+        LocalDate datetoday = LocalDate.now();
+        EmotionDuplicationDto duplication = new EmotionDuplicationDto();
         duplication.setDateToday(String.valueOf(datetoday));
         duplication.setIuser(dto.getIuser());
 
-        EmotionDuplicationVo checkDuplication=emotionRepository.checkDuplicationEmo(duplication);
+        EmotionDuplicationVo checkDuplication = emotionRepository.checkDuplicationEmo(duplication);
 
-        if(checkDuplication!=null){
+        if (checkDuplication != null) {
             throw new BadDateInformationException("하루에 이모션을 중복해서 등록 불가능합니다.");
         }
-        Integer emoTagInt=emotionRepository.tagConvertInteger(dto.getEmoTag());
-        if(emoTagInt==null){
+        Integer emoTagInt = emotionRepository.tagConvertInteger(dto.getEmoTag());
+        if (emoTagInt == null) {
             throw new BadInformationException("태그를 올바르게 작성 해 주세요");
         }
         dto.setEmoTagInt(emoTagInt);
         int result = emotionRepository.postEmo(dto);
-        if(result==0){
+        if (result == 0) {
             throw new RuntimeException("DB 오류");
         }
         return new ResVo(result);
@@ -58,25 +61,25 @@ public class EmotionService {
     //해당한 날에 일정이 있으면 _todo=1, 없으면 _todo=0.
     public List<EmotionSelVo> getEmo(EmotionSelDto dto) {
         // 반복X 일정.
-        List<EmotionSelVo> todo=emotionRepository.getTodoMonth(dto);
+        List<EmotionSelVo> todo = emotionRepository.getTodoMonth(dto);
         // 반복O 일정 추가.
         todo.addAll(emotionRepository.getRepeatTodoMonth(dto));
-        Integer checkIuser= emotionRepository.checkIuser(dto.getIuser());
-        if(checkIuser==null){
+        Integer checkIuser = emotionRepository.checkIuser(dto.getIuser());
+        if (checkIuser == null) {
             throw new NoSuchDataException("올바른 iuser 값을 보내주세요");
         }
         // 중복제거.
-        List<EmotionSelVo> asMonth=todo.stream().distinct()
+        List<EmotionSelVo> asMonth = todo.stream().distinct()
                 .collect(Collectors.toList());
 
         // EmotionGrade,EmotionTag를 등록한 날의 EmotionGrade, 날짜 select.
-        List<EmotionSelVo> emotionMonth=emotionRepository.getEmotionMonth(dto);
+        List<EmotionSelVo> emotionMonth = emotionRepository.getEmotionMonth(dto);
 
-        for (EmotionSelVo todosel:asMonth) {
+        for (EmotionSelVo todosel : asMonth) {
             for (EmotionSelVo emotionsel : emotionMonth) {
                 //_Todo가 적혀진 날들의 리스트와 이모션이 적혀진 날들의 리스트를 탐색.
 
-                if(emotionsel.getDay().equals(todosel.getDay())){
+                if (emotionsel.getDay().equals(todosel.getDay())) {
                     //emotionsel 의 날짜와 _todosel의 날짜가 같으면
 
                     emotionsel.setHasTodo(1);
@@ -89,25 +92,25 @@ public class EmotionService {
                 }
             }
         }
-        int i=0;
-        for (EmotionSelVo emotionSelVo : emotionMonth){
+        int i = 0;
+        for (EmotionSelVo emotionSelVo : emotionMonth) {
 
-            i=emotionSelVo.getHasTodo();
-            if(i==0){
+            i = emotionSelVo.getHasTodo();
+            if (i == 0) {
                 asMonth.add(emotionSelVo);
                 //hasTodo가 0인 emotionMonth을 asMonth에 추가.
             }
         }
         Comparator<EmotionSelVo> byDay = Comparator.comparing(EmotionSelVo::getDay);
-        Collections.sort(asMonth , byDay);
+        Collections.sort(asMonth, byDay);
 
         return asMonth;
     }
 
     // 해당 날의 이모션들 삭제 //
     public ResVo delEmo(EmotionDelDto dto) {
-        Integer checkIuser= emotionRepository.checkIuser(dto.getIuser());
-        if(checkIuser==null){
+        Integer checkIuser = emotionRepository.checkIuser(dto.getIuser());
+        if (checkIuser == null) {
             throw new NoSuchDataException("올바른 iuser 값을 보내주세요");
         }
         int result = emotionRepository.delEmo(dto);
@@ -117,8 +120,8 @@ public class EmotionService {
     // iuser 값을 받아 해당 주의 월요일부터 오늘까지 모든 기록된 감정과
     //감정 태그 의 주별 결산을 select//
     public EmotionSelAsChartVo getEmoChart(int iuser) {
-        Integer checkIuser= emotionRepository.checkIuser(iuser);
-        if(checkIuser==null){
+        Integer checkIuser = emotionRepository.checkIuser(iuser);
+        if (checkIuser == null) {
             throw new NoSuchDataException("올바른 iuser 값을 보내주세요");
         }
 
@@ -127,7 +130,7 @@ public class EmotionService {
 
         //오늘이 일주일의 몇번째 인지 월요일=1,화요일=2,수요일=3
         //목요일=4,금요일=5,토요일=6,일요일=7.
-        int day = today.get(DAY_OF_WEEK)-1;
+        int day = today.get(DAY_OF_WEEK) - 1;
 
         //"오늘 날짜"에서 "오늘이 일주일의 몇번째"를 빼면 요번주의 시작일이 나옴.
         //ex) "2023.12.21 목요일" 이라면 "목요일=3" [12.21 - 3일 = 12월18일 월요일]
@@ -151,7 +154,7 @@ public class EmotionService {
         //weeks 에 emotionSel의 요일을 넣어줌.
         EmotionSelAsChartVo selAsChartVo = new EmotionSelAsChartVo();
         for (int i = 1; i < 8; i++) {
-            if(weeks.contains(i)){
+            if (weeks.contains(i)) {
                 continue;
             }
             EmotionSel emotionSel = new EmotionSel();
--- src/main/java/team6/project/emotion/model/EmotionInsDto.java
+++ src/main/java/team6/project/emotion/model/EmotionInsDto.java
@@ -5,18 +5,21 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import team6.project.common.Const;
+
+import static team6.project.common.Const.*;
 
 @Data
 @Schema(name = "이모션 등록",title = "일 단위의 이모션 등록")
 public class EmotionInsDto {
     @Schema(title = "유저 PK", minimum = "1", type = "int", description = "필수값이며,등록한 유저마다<br>다른iuser값을 가지고 있음.", defaultValue = "유저PK")
     private int iuser;
-    @NotNull
-    @Range(min = 1,max = 5,message = "올바르지 않은 이모션 단계 입력")
+    @NotNull(message = NOT_ENOUGH_INFO_EX_MESSAGE)
+    @Range(min = 1,max = 5,message = BAD_INFO_EX_MESSAGE)
     @Schema(title = "이모션 단계", minimum = "1", maximum = "5",type = "int",description = "이모션 단계를 입력.<br>최소 1~ 최대5 까지.")
     private Integer emoGrade;
     @Schema(title = "이모션 태그",type = "String",description = "이모션 태그를 입력.<br> ex)괴로운,귀찮은,기쁜,즐거운 ... 총 28가지.")
-    @NotBlank(message = "올바르지 않은 이모션 태그 값 입력")
+    @NotBlank(message = BAD_INFO_EX_MESSAGE)
     private String emoTag;
     @JsonIgnore
     private int emoTagInt;
